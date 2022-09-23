2f26b6515b8a64eb02b78f02eddb1146aa13cfce
alex <alenelamesgin@gmail.com>
2022-09-23 04:11:32 -0700

    7. Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code for
  *
@@ -10,7 +9,6 @@ int main(void)
 {
     char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
     char *p;
-
     p = leet(s);
     printf("%s", p);
     printf("%s", s);
